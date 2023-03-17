7085b041de30c30678726475a2d5589e498db94b
kushnerovM <Rojers1941@yandex.by>
2023-03-17 12:01:01 +0300

    feat: animate blocks during scroll

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, ScrollToPlugin);
 
-ScrollSmoother.create({
-    wrapper: ".wrapper",
-    content: ".content",
-    effects : true,
-    normalizeScroll : true
-});
-
 const AID = ['about','projects','contact'];
 //selectors
 let links = document.querySelector(".menu__list").querySelectorAll("a");
+let projectsList = document.querySelectorAll(".project");
 
-links.forEach((li,index)=>li.addEventListener("click",(event)=>{
-    event.preventDefault();
-    gsap.to(window, {duration: 2, scrollTo: '#'+AID[index]});
-}));
+//desctop animations
+if(!ScrollTrigger.isTouch){
+
+    ScrollSmoother.create({
+        wrapper: ".wrapper",
+        content: ".content",
+        effects : true,
+        normalizeScroll : true
+    });
+
+    links.forEach((li,index)=>li.addEventListener("click",(event)=>{
+        event.preventDefault();
+        gsap.to(window, {duration: 1.2, scrollTo: '#'+AID[index]});
+    }));
+
+    gsap.fromTo('.start__image-container', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.start__image-container',
+			start: 'center',
+			end: 'bottom',
+			scrub: true
+		}
+	});
+
+    projectsList.forEach((project,index)=>{
+        gsap.fromTo(project,{
+            opacity: .5,
+            x: index%2?100:-100
+        },{
+            opacity:1,
+            x: 0,
+            scrollTrigger: {
+				trigger: project,
+                start: '-150%',
+				end: 'top',
+				scrub: true
+			}
+        })
+    });
+}
+
